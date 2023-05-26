0e8e9955083b52a4b2c403f8b825275089c5cc9e
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2023-05-26 16:11:51 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -25,29 +25,32 @@ function App() {
   const { company, title, duties, dates } = job[value];
   return (
     <>
-      <div className="App  w-[700px]  mx-auto flex space-x-8 items-center">
+      <div className="App  w-[700px]  mx-auto flex space-x-12 items-center">
         <div className="grid gap-4">
           {
             job.map((i,a)=>{
               return(
                  <div>
-                  <button onClick={()=>setValue(a)}>{i.company}</button>
+                  <button className="hover:border-l-[5px] w-[120px] p-2 border-orange-600 hover:bg-orange-200" onClick={()=>setValue(a)}>{i.company}</button>
                  </div>
               )
             })
           }
         </div>
         <div className="  w-[700px]  h-[600px] mx-auto grid place-content-center  p-5 gap-4">
-          <p className="text-5xl text-center">Experience</p>
+         <div className="grid gap-4 place-content-center">
+            <p className="text-5xl text-center">Experience</p>
+            <div className="w-[100px] h-[3px] ml-16  rounded-lg bg-blue-500"></div>
+         </div>
 <p className="text-3xl font-semibold">{title}</p>
 <p className="text-xl bg-slate-500/30 text-white rounded-md text-center w-[90px] p-2">{company}</p>
 <p className="text-slate-400 text-2xl">{dates}</p>
     {
       duties.map((i,index)=>{
         return(
-          <div key={index} className=" flex ">
-            <AiFillRightCircle size={30} />
-             {i}
+          <div key={index} className=" flex  space-x-3 ">
+            <AiFillRightCircle size={50} />
+            <p>{i}</p>
               
               
               
